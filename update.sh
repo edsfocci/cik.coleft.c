501b1c5b467ee1fab985218dd032beaf0b13f53e
Ed Solis <edsfocci@gmail.com>
2014-08-18 17:41:41 +0000

    cik.coleft.c on 2014-08-18

--- update.sh
+++ update.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-wget -q http://www.sec.gov/edgar/NYU/cik.coleft.c.back -O cik.coleft.c
+wget -q http://www.sec.gov/edgar/NYU/cik.coleft.c -O cik.coleft.c
 
 function git_commit {
-  local NOW="2014-08-11"
-#  local NOW=$(date +%Y-%m-%d)
+  local NOW=$(date +%Y-%m-%d)
   git add .
   git commit -m "cik.coleft.c on $NOW"
-#  git push
+  git push
 }
 
 git_commit

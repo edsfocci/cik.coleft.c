c6b219a8527fe03c04fc0b1f8acb7084de2fba96
Ed <edsfocci@gmail.com>
2014-09-01 06:16:23 +0000

    cik.coleft.c on 2014-09-01

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-wget -q http://www.sec.gov/edgar/NYU/cik.coleft.c -O cik.coleft.c
+wget -q http://www.sec.gov/edgar/NYU/cik.coleft.c -O /home/ed/Bash/cik.coleft.c/cik.coleft.c
 
 function git_commit {
+  cd /home/ed/Bash/cik.coleft.c/
   local NOW=$(date +%Y-%m-%d)
   git add .
   git commit -m "cik.coleft.c on $NOW"
   git push
+  cd /home/ed/
 }
 
 git_commit
